8ad5d3b515b5a61e57282c0da46082933f6ac492
Jacek Okrojek <jacek.okrojek@testarmy.com>
2020-01-30 10:10:17 +0100

    Initial commit

---
+++ excercises/4-pop/contact.spec.ts
@@ -0,0 +1,12 @@
+describe('Protractor Workshop app', function() {
+
+	beforeEach(function () {
+	});
+
+	xit('should display text "Your message has been sent." when user sends a message', function(){
+	});
+
+	xit('should have "Get in touch" and "Address" sections on Contact Us page', function(){
+	});
+
+});
